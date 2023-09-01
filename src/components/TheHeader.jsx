1810a04d3861f896e6a4f68291bf86fc9429d0ec
Harsh Morayya <harshmorayya@ymail.com>
2023-09-01 20:52:54 +0530

    Added Header

--- src/components/TheHeader.jsx
+++ src/components/TheHeader.jsx
@@ -1,8 +1,50 @@
+import "../styles/header.css";
+
 const TheHeader = () => {
   return (
     <div>
-      <header>
-        <h1>Placeholder Header</h1>
+      <header className="header-container">
+        <div className="logo">
+          <h2>
+            CODE<span style={{ color: "red" }}>&#123;X&#125;</span>
+          </h2>
+        </div>
+        <input type="checkbox" id="nav_check" hidden />
+        <nav className="main-nav">
+          {/* <div className="logo">
+            <img src="Logo.jpg" />
+          </div> */}
+          <ul>
+            <li>
+              <a href="" className="active">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="">About</a>
+            </li>
+            <li>
+              <a href="">Blog</a>
+            </li>
+            <li>
+              <a href="">Events</a>
+            </li>
+            <li>
+              <a href="">Portfolio</a>
+            </li>
+            <li>
+              <a href="">Team</a>
+            </li>
+            <li>
+              <a href="">Contact</a>
+            </li>
+          </ul>
+        </nav>
+        <label htmlFor="nav_check" className="hamburger">
+          <div></div>
+          <div></div>
+          <div></div>
+        </label>
       </header>
     </div>
   );
