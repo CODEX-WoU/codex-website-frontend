94a22291a20598e515735991daebbe23b9458908
Harsh00988 <goyalharsh300@gmail.com>
2023-09-03 15:43:00 +0530

    New changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { Routes, Route, Outlet, Link } from "react-router-dom";
 import "./styles/App.css";
 import TheHeader from "./components/TheHeader";
 import TheFooter from "./components/TheFooter";
+import LandingPage from "./components/LandingPage";
 
 function App() {
   return (
     <>
       <TheHeader />
+      <LandingPage/>
       <TheFooter />
     </>
   );
