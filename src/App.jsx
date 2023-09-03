6ff66e14c9aae53dbc3a0b95e74e830e5a53815f
Harsh00988 <goyalharsh300@gmail.com>
2023-09-04 01:19:56 +0530

    Added Events but not completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import "./styles/App.css";
 import TheHeader from "./components/TheHeader";
 import TheFooter from "./components/TheFooter";
 import LandingPage from "./components/LandingPage";
+import Events from "./components/Events";
 
 function App() {
   return (
     <>
       <TheHeader />
       <LandingPage/>
+      <Events />
       <TheFooter />
     </>
   );
