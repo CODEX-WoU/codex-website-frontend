c64d1dfa28fea9a7b3c891ac8cda30bb385233b1
Harsh Morayya <harshmorayya@ymail.com>
2023-09-03 15:52:52 +0530

    Removed max-width

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,21 +1,19 @@
-import "../styles/landingpage.css"
+import "../styles/landingpage.css";
 
-const LandingPage = () => { 
-    return (
-        <div>
-            {/*create a grid layout with 3 rows and 3 columns and width should be full screen in tailwind css */}
-            <div className="grid grid-cols-3 gap-4">
-                <div className="bg-red-500 rounded-lg shadow-xl min-h-[50px]"></div>
-                <div className="bg-red-500 rounded-lg shadow-xl min-h-[50px]"></div>
-                <div className="bg-red-500 rounded-lg shadow-xl min-h-[50px]"></div>
-                <div className="bg-red-500 rounded-lg shadow-xl min-h-[50px]"></div>
-                <div className="bg-red-500 rounded-lg shadow-xl min-h-[50px]"></div>
-                <div className="bg-red-500 rounded-lg shadow-xl min-h-[50px]"></div>
-            </div>
+const LandingPage = () => {
+  return (
+    <div>
+      {/*create a grid layout with 3 rows and 3 columns and width should be full screen in tailwind css */}
+      <div className="grid grid-cols-3 gap-4">
+        <div className="bg-red-500 rounded-lg shadow-xl min-h-[50px]"></div>
+        <div className="bg-red-500 rounded-lg shadow-xl min-h-[50px]"></div>
+        <div className="bg-red-500 rounded-lg shadow-xl min-h-[50px]"></div>
+        <div className="bg-red-500 rounded-lg shadow-xl min-h-[50px]"></div>
+        <div className="bg-red-500 rounded-lg shadow-xl min-h-[50px]"></div>
+        <div className="bg-red-500 rounded-lg shadow-xl min-h-[50px]"></div>
+      </div>
+    </div>
+  );
+};
 
-        </div>
-    );
-
-}
-
-export default LandingPage
+export default LandingPage;
