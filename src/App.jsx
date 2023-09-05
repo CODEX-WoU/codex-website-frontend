72d73ef1880bd4c256747005ed879a5451c19d13
Harsh Morayya <harshmorayya@ymail.com>
2023-09-05 10:36:16 +0530

    Added Contacts back to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,10 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import "./styles/App.css";
 import TheHeader from "./components/TheHeader";
 import TheFooter from "./components/TheFooter";
-// import Events from "./components/Events";
+import Events from "./components/Events";
 import LandingPageAlt from "./components/LandingPageAlt";
-<<<<<<< HEAD
 import Contact from "./components/Contact";
-=======
 import EventsAlt from "./components/EventsAlt";
->>>>>>> ba521ed (Added EventsAlt)
 
 function App() {
   return (
@@ -22,12 +19,8 @@ function App() {
               element={
                 <div>
                   <LandingPageAlt />
-<<<<<<< HEAD
-                  <Contact />
-                  {/* <Events /> */}
-=======
                   <EventsAlt />
->>>>>>> ba521ed (Added EventsAlt)
+                  <Contact />
                 </div>
               }
             />
